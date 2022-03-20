91d1d6cb7120618a8309862e3276443d0653144b
root <root@DESKTOP-1BAE7AU.localdomain>
2022-03-20 14:10:08 -0300

    React Routes v6 is quite different from the lesson. I had to make some adjusts

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
-
+import { Routes, Route, Navigate, Router } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -17,18 +18,27 @@ class Main extends Component {
       };
     }
 
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
-    }
+   
 
     render() {
+
+    const HomePage =  () => {
+      return(
+        <Home />
+      );
+    }
+  
+
     return (
       <div className="Main">
         <Header />
-        <Menu dishes={this.state.dishes} 
-            onClick={(dishId)=>this.onDishSelect(dishId)} />
-        <DishDetail 
-            dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route exact path="/menu" element={<Menu dishes={this.state.dishes} />} />
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        
         <Footer />
       </div>
     );
