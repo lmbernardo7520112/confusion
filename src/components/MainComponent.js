8060f06036d0faa82f8791cc23618c8ab7be36e2
root <root@DESKTOP-1BAE7AU.localdomain>
2022-03-25 00:08:08 -0300

    SPA Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
+import Contact from './ContactComponent';
+//import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Routes, Route, Navigate } from 'react-router-dom';
+
 
 class Main extends Component {
 
@@ -14,34 +19,36 @@ class Main extends Component {
 
       this.state = {
         dishes: DISHES,
-        selectedDish: null
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
       };
     }
 
-   
-
     render() {
 
-    const HomePage =  () => {
-      return(
-        <Home />
-      );
-    }
+      const HomePage =  () => {
+        return(
+          <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
+           promo={this.state.promotions.filter((promo) => promo.featured)[0]}
+           leader={this.state.leaders.filter((leader) => leader.featured)[0]} 
+          />
+        );
+      }
   
 
-    return (
-      <div className="Main">
-        <Header />
-        
-          <Routes>
-            <Route path="/home" element={<HomePage />} />
-            <Route exact path="/menu" element={<Menu dishes={this.state.dishes} />} />
-            <Route path="*" element={<Navigate to="/home" />} />
-          </Routes>
-        
-        <Footer />
-      </div>
-    );
+      return (
+        <div className="Main">
+          <Header />
+            <Routes>
+              <Route path="/home" element={<HomePage />} />
+              <Route exact path="/menu" element={<Menu dishes={this.state.dishes} />} />
+              <Route path="*" element={<Navigate to="/home" />} />
+              <Route exact path="/contactus" element={<Contact />} />
+            </Routes>
+          <Footer />
+        </div>
+      );
   } 
 }
 
