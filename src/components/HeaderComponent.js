3c6a2067350b085713e14efdc7786fc7f21c333c
root <root@DESKTOP-1BAE7AU.localdomain>
2022-04-24 16:05:13 -0300

    Uncontrolled Form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem,
-    Button, Modal, ModalHeader, ModalBody} from 'reactstrap';
+    Button, Form, Label, Modal, ModalHeader, ModalBody, FormGroup, Input} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -26,6 +26,13 @@ class Header extends Component {
             isModalOpen: !this.state.isModalOpen
         });
 
+    }
+
+    handleLogin(event) {
+        this.toggleModal(); 
+        alert("Username: " + this.username.value + "Password: " + this.password.value
+        + "Remember: " + this.remember.checked);
+        event.preventDefault();
     }
 
     render () {
@@ -84,7 +91,26 @@ class Header extends Component {
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
                     <ModalBody>
-
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username" 
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password" 
+                                    innerRef={(input) => this.password = input} />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember" 
+                                        innerRef={(input) => this.remember = input} />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color='primary'>Login</Button>
+                        </Form>
                     </ModalBody>
                 </Modal>
             </>
